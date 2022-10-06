c194204588280f70825a50c21aa98f8ce0a6ac3a
miniece <mvelinda@icloud.com>
2022-10-06 17:25:57 -0400

    First commit for lab

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    internal class Circle
+    {
+        //properties
+        public double radius { get; set; }
+
+        //constructor
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double CalculateCircumference(double radius)
+        {
+            double circumference = 2 * Math.PI * radius;
+            return Math.Round(circumference, 2);
+        }
+        public double CalculateArea(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return Math.Round(area, 2);
+        }
+
+
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CircleObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goOn = true;
+            while (goOn == true)
+            {
+                double radius;
+                try
+                {
+                    Console.WriteLine("Please enter a radius.");
+                    radius = double.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("You must enter a positive integer.");
+                    continue;
+                }
+                //calling class and printing out numbers
+                Circle c = new Circle(radius);
+               
+                c.CalculateCircumference(radius);
+                Console.WriteLine($"Circumference: {c.CalculateCircumference(radius)}");
+                c.CalculateArea(radius);
+                Console.WriteLine(($"Area: {c.CalculateArea(radius)}"));
+
+
+                goOn = goAgain();
+            }
+        }
+        public static bool goAgain()
+        {
+            int goCounter = 1;
+            goCounter++;
+            Console.WriteLine("Measure another circle?");
+            string input = Console.ReadLine().ToLower();
+                
+            if (input == "y")
+            {
+                return true;
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("Goodbye. You have created " + goCounter + " circle object(s).");  //"You have created" + goAgain counter + circle object(s)
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Please enter Y or N");
+                return goAgain();
+            }
+        }
+    }
+}
